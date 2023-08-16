5f50c37354a8e4f0efe3b3cf8c83319a4985aa00
snoopyods <snoopyods@gmail.com>
2023-08-16 10:10:01 -0700

    Added table 'flask db upgrade'

--- app/models.py
+++ app/models.py
@@ -31,7 +31,7 @@ class Post(db.Model):
     body = db.Column(db.String, nullable=False)
     image_url = db.Column(db.String, nullable=False, default=random_photo)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"<Post {self.id}|{self.title}>"
