ce1e604a3c965e1938e1cba627677e4e2990a2fc
snoopyods <snoopyods@gmail.com>
2023-08-17 12:23:26 -0700

    modified create post function to allow posts into db

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,7 @@
 from . import api
 from app import db
 from app.models import Post
+from flask import request
 
 
 @api.route('/posts')
@@ -15,3 +16,27 @@ def get_post(post_id):
         return post.to_dict()
     else:
         return {'error': f'Post with an ID of {post_id} does not exist'}, 404
+    
+@api.route('/posts', methods=["POST"])
+def create_post():
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    data = request.json
+    required_fields = ['title', 'body']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+    
+    title = data.get('title')
+    body = data.get('body')
+    image_url = data.get('image_url')
+
+    new_post = Post(title=title, body=body, image_url=image_url, user_id=2)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return new_post.to_dict(), 201
